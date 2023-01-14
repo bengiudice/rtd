581555b872b7a606edee8fb8acefcdb9041047f7
benjamin giudice <ben.giudice@gmail.com>
2023-01-14 22:25:25 +0800

    Add system

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 use bevy::prelude::*;
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_system(hello_world)
+        .run();
+}
+
+fn hello_world() {
+    println!("Hello world!");
 }

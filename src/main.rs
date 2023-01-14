db0259135fb66ec15e5e298c6310b7604f6684ee
benjamin giudice <ben.giudice@gmail.com>
2023-01-14 23:46:54 +0800

    Limit log rate; refactor as plugin

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,31 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_system(hello_world)
+        .add_plugin(HelloPlugin)
         .run();
 }
-
-fn hello_world() {
-    println!("Hello world!");
+pub struct HelloPlugin;
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}
+#[derive(Component)]
+struct Person;
+#[derive(Component)]
+struct Name(String);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("hi".into())));
+    commands.spawn((Person, Name("u".into())));
+}
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0)
+        }
+    }
 }
